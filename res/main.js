97072e2228939f42ef0d60da9118eda2724df572
IMO <shfn7253@gmail.com>
2026-03-18 09:37:23 +0900

    Taa

--- res/main.js
+++ res/main.js
@@ -123,15 +123,18 @@ function AAA(){
 		url: 'https://192.168.1.134/GetData.php',
 		dataType: 'json',
 	}).done((data, textStatus, jqXHR) => {
+		/*
 		ActDB("INSERT",{
 			"NAME":"AAA",
 			"VALUE":"data:application/pdf;base64," + data.value
-		});
+		});*/
+		console.log(textStatus);
+		console.log(jqXHR);
 		AddLog("正常終了");
 	}).fail((jqXHR, textStatus, errorThrown) => {
 		// 異常処理
 		AddLog("異常終了");
-		AddLog(textStatus);
+		AddLog(jqXHR.responseJSON.Message);
 	});
 }
 
